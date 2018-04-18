3a82c00f0e8a4f69c464d6eb70add0b0e98be44c
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-17 22:33:53 -0300

    add header into the app

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+
+import store from '../../store';
+import Header from '../header';
+
 
 const App = props => (
-    <div>App</div>
+  <Provider store={store}>
+    <Header />
+  </Provider>
 );
 
 export default App;
