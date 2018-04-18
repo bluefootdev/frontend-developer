4328c08a251b35b75d8213b4b8ee3a66b6c1b55e
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-18 00:53:35 -0300

    use suggestedproducitems with items of the first item found at autocomplete

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { searchIfNeeded } from '../../store/reducers/products';
 import Autocomplete from './autocomplete';
 import SuggestedNamesList from './suggested-names-list';
+import SuggestedProductItems from './suggested-product-items';
 
 
 const Header = (props) => (
@@ -14,6 +15,10 @@ const Header = (props) => (
       props.suggestedNames &&
       <SuggestedNamesList suggestedNames={props.suggestedNames} />
     }
+    {
+      props.suggestedProducts &&
+      <SuggestedProductItems products={props.suggestedProducts} />
+    }
   </header>
 );
 
@@ -21,7 +26,7 @@ const mapStateToProps = state => {
   const {suggestedProducts} = state.product;
   return {
     suggestedNames: suggestedProducts.map(product => product.name),
-    suggestedProducts
+    suggestedProducts: suggestedProducts.length ? suggestedProducts[0].items : []
   }
 }
 
