278aefd5be5a7d94e148ac7a4d67518148f261f0
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-17 21:35:35 -0300

    change structure to reuse code

--- src/api/index.js
+++ src/api/index.js
@@ -2,15 +2,26 @@ import fetch from 'cross-fetch';
 
 const AUTOCOMPLETE_URL = 'http://agenciabluefoot.vtexcommercestable.com.br/buscaautocomplete/?productNameContains=';
 
+const headers = new Headers({
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': 'true'
+});
+const options = {
+  method: 'GET',
+  headers,
+  mode: 'cors',
+  cache: 'default'
+};
+
 const searchProducts = async text => {
   if(!text) {
     return {};
   }
   try {
-    const response = await fetch(`${AUTOCOMPLETE_URL}${text}`)
+    const response = await fetch(`${AUTOCOMPLETE_URL}${text}`, options);
     return response.json();
   } catch(e) {
-    console.error('Error in searchProducts: ', e);
+    throw e;
   }
 }
 
