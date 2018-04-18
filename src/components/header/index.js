cf587ab8208517f8bb0a4bb00d8228754f205c2c
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-18 00:36:43 -0300

    use suggestednameslist

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,15 +4,26 @@ import { connect } from 'react-redux';
 
 import { searchIfNeeded } from '../../store/reducers/products';
 import Autocomplete from './autocomplete';
+import SuggestedNamesList from './suggested-names-list';
 
 
-const Header = ({products, searchIfNeeded}) => (
+const Header = (props) => (
   <header className="site-header">
-    <Autocomplete onChange={searchIfNeeded} />
+    <Autocomplete onChange={props.searchIfNeeded} />
+    {
+      props.suggestedNames &&
+      <SuggestedNamesList suggestedNames={props.suggestedNames} />
+    }
   </header>
 );
 
-const mapStateToProps = state => ({products: state.product.products})
+const mapStateToProps = state => {
+  const {suggestedProducts} = state.product;
+  return {
+    suggestedNames: suggestedProducts.map(product => product.name),
+    suggestedProducts
+  }
+}
 
 const mapDispatchToProps = dispatch => (
   bindActionCreators({ searchIfNeeded }, dispatch)
