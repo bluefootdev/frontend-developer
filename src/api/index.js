83f6bd6baefdef6856f73a23392431f707487ced
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-18 21:20:33 -0300

    fix return of search functions and comment some options of fetch because i couldnt resolve cors

--- src/api/index.js
+++ src/api/index.js
@@ -5,12 +5,12 @@ const AUTOCOMPLETE_URL = 'buscaautocomplete/?productNameContains=';
 const FULL_TEXT_URL = 'api/catalog_system/pub/products/search/';
 
 const options = {
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
-  },
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Content-Type': 'application/json'
+  // },
   method: 'GET',
-  mode: 'cors',
+  // mode: 'cors',
 };
 
 const request = async (url) => {
@@ -24,13 +24,13 @@ const request = async (url) => {
 
 export const searchProducts = async text => (
   !text
-  ? {}
+  ? []
   : request(`${AUTOCOMPLETE_URL}${text}`)
       .then(response => response.itemsReturned)
 );
 
 export const searchFullTextProducts = async text => (
-  !text ? {} : request(`${FULL_TEXT_URL}${text}?map=ft`)
+  !text ? [] : request(`${FULL_TEXT_URL}${text}?map=ft`)
 );
 
 export const goToProduct = ({link}) => {
