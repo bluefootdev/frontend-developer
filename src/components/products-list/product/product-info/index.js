5d2bc1dd9353c007419c78152168ce7b76197f0f
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-18 22:12:12 -0300

    add getminprice function that compares sellers commertial offers to show minimum value of product

--- src/components/products-list/product/product-info/index.js
+++ src/components/products-list/product/product-info/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const ProductInfos = ({productName, description}) => (
+const ProductInfos = ({ name, sellers }) => (
   <div>
-    <h3>{productName}</h3>
-    <p>{description}</p>
+    <h3>{name}</h3>
+    <p>Min. Price: ${getMinPrice(sellers)}</p>
   </div>
 );
+
+const getMinPrice = (sellers) => (
+  sellers.reduce((price, seller) => {
+    const priceComparation = seller.commertialOffer.Price;
+    return price !== null && price <= priceComparation ? price : priceComparation;
+  }, null)
+)
 
 export default ProductInfos;
