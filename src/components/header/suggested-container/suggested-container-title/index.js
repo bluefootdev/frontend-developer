c895ca1090dfde4b947180b25c684b1a94c51d83
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-19 21:35:54 -0300

    add title component

---
+++ src/components/header/suggested-container/suggested-container-title/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './index.css';
+
+
+const SuggestedContainerTitle = ({title}) => (
+  <h3 className="suggested-container-title">
+    {title}
+  </h3>
+);
+
+export default SuggestedContainerTitle;
