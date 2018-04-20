84e1ba91d04dfac87cf07f9ad94a296a3948194c
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-19 22:07:29 -0300

    add function that hide suggestions when click outside of component

--- src/components/header/suggested-container/index.js
+++ src/components/header/suggested-container/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import onClickOutside from "react-onclickoutside";
 
 import SuggestedNamesList from './suggested-names-list';
 import SuggestedProductItems from './suggested-product-items';
 import SuggestedContainerTitle from './suggested-container-title';
+import { showSuggestions } from '../../../store/reducers/products';
 
 import './index.css';
 
 
-const SuggestedContainer = ({ suggestedNames, suggestedProducts }) => (
+const SuggestedContainer = onClickOutside(({ suggestedNames, suggestedProducts }) => (
   <div className="suggested-container">
     {
       suggestedNames &&
@@ -24,6 +28,14 @@ const SuggestedContainer = ({ suggestedNames, suggestedProducts }) => (
       </div>
     }
   </div>
-);
+));
 
-export default SuggestedContainer;
+SuggestedContainer.prototype.handleClickOutside = function(event) {
+  this.props.showSuggestions(false);
+}
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({ showSuggestions }, dispatch)
+)
+
+export default connect(null, mapDispatchToProps)(SuggestedContainer);
