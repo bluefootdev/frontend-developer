1e31bac938d8bb6cae8e3c14e1c34bd69c1a3923
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-19 21:36:15 -0300

    move file and add onclick event

---
+++ src/components/header/suggested-container/suggested-product-items/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { getProductsFullText } from '../../../../store/reducers/products';
+import './index.css';
+
+
+const SuggestedProductItems = ({ products = [], getProductsFullText }) => {
+  return (
+    <div className="suggested-products">
+      {
+        products.map(product => (
+          <div
+            key={product.itemId}
+            className="suggested-product"
+            onClick={() => getProductsFullText(product.nameComplete)}
+          >
+            <img
+              className="suggested-product-image"
+              src={product.imageUrl}
+              alt={product.name}
+            />
+            <h5>{product.name}</h5>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({ getProductsFullText }, dispatch)
+)
+
+export default connect(null, mapDispatchToProps)(SuggestedProductItems);
