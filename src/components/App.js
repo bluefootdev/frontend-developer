db0a7273bd2b37da35d7264c16ac07ffa0766f82
Charliston Maffei <charliston_m@yahoo.com.br>
2018-04-21 12:14:51 +0100

    Install dependency: react-loadable

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Switch, Route } from "react-router-dom";
+import * as Routes from './routes';
 import Navbar from './Header/Navbar';
 import Featured from './Home/Featured';
 
@@ -8,8 +10,13 @@ class App extends Component {
       <div className="App">
         <Navbar/>
 
-        <Featured/>
-
+        <Switch>
+          <Route exact path='/' component={Routes.ShowList}/>
+          <Route exact path='/shows' component={Routes.ShowList}/>
+          <Route path='/shows/add' component={Routes.ShowAdd}/>
+          <Route path='/shows/:showId/edit' component={Routes.ShowEdit}/>
+          <Route path='/shows/:showId' component={Routes.ShowInfo}/>
+        </Switch>
       </div>
     );
   }
