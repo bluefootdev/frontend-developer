4ba4dfe6e075e0263024016393b9397714b2d95f
Charliston Maffei <charliston_m@yahoo.com.br>
2018-04-21 12:23:35 +0100

    Fisrt version of routes: index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import { Router } from "react-router-dom";
+import History from './utils/history';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={History}>
+    <App />
+  </Router>
+  , document.getElementById('root'));
