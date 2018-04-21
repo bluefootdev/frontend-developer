b9553f53aa512412894cf4fc5533c0a3eca2338c
Charliston Maffei <charliston_m@yahoo.com.br>
2018-04-21 12:02:33 +0100

    Mockup components in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './Header/Navbar';
+import Featured from './Home/Featured';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App started
+        <Navbar/>
+
+        <Featured/>
+
       </div>
     );
   }
