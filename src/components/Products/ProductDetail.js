51ae0eb0b79a9ed25da95c42d9c73e78b9045dcd
Charliston Maffei <charliston_m@yahoo.com.br>
2018-04-21 12:02:21 +0100

    Mockup components: product detail

---
+++ src/components/Products/ProductDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class ProductDetail extends Component {
+  render() {
+    return (
+      <div>
+        A product here…
+      </div>
+    );
+  }
+}
+
+export default ProductDetail;
