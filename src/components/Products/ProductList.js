d27aeadb2343b68824b45b435bbcdbc35c2da11f
Charliston Maffei <charliston_m@yahoo.com.br>
2018-04-26 17:19:20 -0300

    ProductList mock

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../../styles/productList.css';
 
 export class ProductList extends Component {
   render() {
     return(
-      <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 col-sm-6 col-md-4 col-lg-3">
+            <Link to="/" className="col-item">
+              <div className="photo">
+                <img src="http://agenciabluefoot.vteximg.com.br/arquivos/ids/155401/amarela.jpg?v=636029709278930000" className="img-responsive" alt="Product" />
+                <span className="badge badge-primary">Kit</span>
+              </div>
+              <div className="info">
+                <div className="row">
+                  <div className="price-details col">
+                    <h1>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur</h1>
+                    <span className="price-new">$110.00</span>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
 
+        </div>
       </div>
     );
   }
